8ba1ce8434751498e77f5460cec8fe5060541552
rkh4 <rhys_howard@msn.com>
2020-05-09 16:15:33 +0100

    Updates

---
+++ precache-manifest.34d06968dfd40a589d6b2182aa0a12e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e56ab0097b4571fe660c3f8ac97715a",
+    "url": "/howardhavens/index.html"
+  },
+  {
+    "revision": "9a92fe9125966aea411a",
+    "url": "/howardhavens/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "573219dfb9b4200123f1",
+    "url": "/howardhavens/static/js/2.44352c30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/howardhavens/static/js/2.44352c30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a92fe9125966aea411a",
+    "url": "/howardhavens/static/js/main.fe7f9ba3.chunk.js"
+  },
+  {
+    "revision": "592f61e26f2b718b77c8",
+    "url": "/howardhavens/static/js/runtime-main.8a98c75e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/howardhavens/static/media/logo.5d5d9eef.svg"
+  }
+]);
