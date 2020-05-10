00eac1c3a8218f2ef8338194547481da2043d1a4
rkh4 <rhys_howard@msn.com>
2020-05-10 17:56:42 +0100

    Added drop down arrow, transition to second page effect, started on hover effect

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import house1 from '../house1.jpeg';
 import logoWhite from '../logoWhite.svg';
+import downArrow from '../downArrow.svg';
 
 const Section = styled.div`
     height: 100vh;
@@ -35,14 +36,32 @@ const MainText = styled.div`
     width: 100%;
 `;
 
+const DownArrow = styled.a`
+    background-image: url(${downArrow});
+    background-size: 100%;
+    background-position-y: center;
+    background-repeat: no-repeat;
+    width: 4%;
+    height: 10%;
+    position: absolute;
+    bottom: 5px;
+    left: 48%;
+
+    &:hover {
+        transition: all 0.3s ease;
+        /* transform: translate(0, -10px) scale(1.1); */
+    }
+`;
+
 const Home: React.FC = () => {
     return (
     <>
         <LandingSection>
             <MainLogo />
             <MainText>HOWARD HAVENS</MainText>
+            <DownArrow href="#section2"/>
         </LandingSection>
-        <Section>
+        <Section id="section2">
           Test
         </Section>
     </>
