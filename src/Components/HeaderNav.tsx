8d6baf4dfbcf723ecca29d5072b8c78dd3798f93
rkh4 <rhys_howard@msn.com>
2020-06-13 12:25:41 +0100

    Header components, and header logo sorted

--- src/Components/HeaderNav.tsx
+++ src/Components/HeaderNav.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
-// import logoWhite from '../logoWhite.svg';
+import logoWhite from '../logoWhite.svg';
 import styled from 'styled-components';
 import { colours } from '../Shared/SharedStyles';
 
 const HeaderNavContainer = styled.div`
     width: 100%;
     max-width: 1800px;
-    height: 80px;
+    height: 100px;
     display: flex;
     justify-content: flex-end;
     align-items: center;
+
+    & > img {
+        position: absolute;
+        top: calc(100vh + 15px);
+        left: 50px;
+        height: 60px;
+    }
 `;
 
 const HeaderButton = styled.button`
@@ -34,13 +41,12 @@ const HeaderButton = styled.button`
 const HeaderNav: React.FC = () => {
     return (
         <HeaderNavContainer>
-            {/* <img src={logo} alt="Howard Havens Logo" /> */}
-            <HeaderButton>ABOUT</HeaderButton>
+            <img src={logoWhite} alt="Howard Havens Logo" />
+            <HeaderButton>ABOUT US</HeaderButton>
             <HeaderButton>LETTINGS</HeaderButton>
             <HeaderButton>DEAL SOURCING</HeaderButton>
-            <HeaderButton>ABOUT US</HeaderButton>
+            <HeaderButton>OTHER SECTION</HeaderButton>
             <HeaderButton>CONTACT</HeaderButton>
-            {/* <HeaderButton>Button1</HeaderButton> */}
         </HeaderNavContainer>
     );
 }
